ad8923ddb02c12e1732675039dd2c45f46ac6b02
Kamate-Idris <kat.dris13@gmail.com>
2023-12-12 21:51:17 +0000

    Initail commit

---
+++ src/main/java/com/kamate/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.kamate.movies;
+
+
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
